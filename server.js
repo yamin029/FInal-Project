64a180ebb4153c442749fb17e28eaf2e616c5d25
sajid9505 <42285255+sajid9505@users.noreply.github.com>
2019-11-28 09:55:11 +0600

    something

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ var app = express();
 var server = http.Server(app);
 const CURRENT_WORKING_DIR = process.cwd()
 var path = require('path');
+var mongo = require('mongodb')
 
+var db, uri = "mongodb+srv://sajid9505:mongoDBatlas@cluster0-ebrcf.mongodb.net/test?retryWrites=true&w=majority"
+
+mongo.MongoClient.connect(uri, 
+    {useNewUrlParser:true, useUnifiedTopology: true }, 
+    function(err, client){
+        if(err){
+            console.log('Could not connect to MongoDB')
+        }else{
+            db = client.db('E-commerce')
+            console.log('Connected')
+        }
+    })
 
 
 app.use('/public', express.static(path.join(CURRENT_WORKING_DIR, 'public')))
