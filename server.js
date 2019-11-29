e1e4b86ebf0bb2215837991966d104a75d44e9a7
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2019-11-29 19:46:29 +0600

    pop-up Chat added

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ var app = express();
 var server = http.Server(app);
 const CURRENT_WORKING_DIR = process.cwd()
 var path = require('path');
+const io = require('socket.io')(3000)
 
+const users = {}
 
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
 
 app.use('/public', express.static(path.join(CURRENT_WORKING_DIR, 'public')))
 
@@ -18,10 +33,6 @@ app.get('/add',function(request,response){
 app.get('/cart',function(request,response){
       response.sendFile(__dirname + '/views/cart.html')
 })
-
-
-
-
 server.listen(3000,'localhost',function(){
       console.log('server running ...')
 })
